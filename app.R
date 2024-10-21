6fdbc639537f22db4dfa73db40554c4880fb7d55
DatViseR <dzdzstepel@gmail.com>
2024-10-21 19:43:41 +0200

    Added data input handled by readr

--- app.R
+++ app.R
@@ -7,44 +7,88 @@
 #    https://shiny.posit.co/
 #
 
+
 library(shiny)
+library(readr)
 
-ui <- fluidPage(titlePanel("Upload CSV File"),
-                sidebarLayout(
-                  sidebarPanel(
-                    fileInput(
-                      "file1",
-                      "Upload a csv file with 3 columns: -log10 pvalue,
-                log2 fold and single gene names or uniprotIDs",
-                      accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")
-                    ),
-                    tags$hr(),
-                    checkboxInput("header", "Header", TRUE),
-                    radioButtons(
-                      "sep",
-                      "Separator",
-                      choices = c(
-                        Comma = ",",
-                        Semicolon = ";",
-                        Tab = "\t"
-                      ),
-                      selected = ","
-                    ),
-                    actionButton("upload", "Upload")
-                  ),
-                  
-                  mainPanel(tableOutput("contents"))
-                ))
+ui <- fluidPage(
+  titlePanel("Advanced Data Input"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Upload a CSV or TSV file",
+                accept = c(".csv", ".tsv")),
+      tags$hr(),
+      checkboxInput("header", "Header", TRUE),
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      radioButtons("dec", "Decimal Point",
+                   choices = c(Dot = ".",
+                               Comma = ","),
+                   selected = "."),
+      actionButton("upload", "Upload"),
+      uiOutput("column_select_ui"),
+      actionButton("save_columns", "Save Columns")
+    ),
+    mainPanel(
+      tableOutput("contents"),
+      verbatimTextOutput("column_info"),
+      verbatimTextOutput("column_structure"),
+      verbatimTextOutput("dataset_summary")
+    )
+  )
+)
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
   observeEvent(input$upload, {
     req(input$file1)
     in_file <- input$file1
-    df <- read.csv(in_file$datapath,
-                   header = input$header,
-                   sep = input$sep)
+    
+    # Use readr package to read the uploaded dataset
+    df <- read_delim(in_file$datapath, delim = input$sep, 
+                     col_names = input$header, locale = locale(decimal_mark = input$dec))
+    
     output$contents <- renderTable({
       head(df)
+    })
+    
+    output$column_select_ui <- renderUI({
+      if (is.null(df)) return(NULL)
+      colnames <- names(df)
+      tagList(
+        selectInput("pvalue_col", "Select pvalue column", choices = colnames),
+        selectInput("fold_col", "Select fold column", choices = colnames),
+        selectInput("annotation_col", "Select annotation column", choices = colnames)
+      )
+    })
+    
+    output$dataset_summary <- renderPrint({
+      summary(df)
+    })
+  })
+  
+  observeEvent(input$save_columns, {
+    req(input$pvalue_col, input$fold_col, input$annotation_col)
+    output$column_info <- renderPrint({
+      list(
+        pvalue = input$pvalue_col,
+        fold = input$fold_col,
+        annotation = input$annotation_col
+      )
+    })
+    
+    output$column_structure <- renderPrint({
+      req(input$file1)
+      in_file <- input$file1
+      
+      df <- read_delim(in_file$datapath, delim = input$sep, 
+                       col_names = input$header, locale = locale(decimal_mark = input$dec))
+      
+      selected_df <- df[, c(input$pvalue_col, input$fold_col, input$annotation_col)]
+      str(selected_df)
     })
   })
 }
