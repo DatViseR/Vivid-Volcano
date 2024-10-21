9b865df98b1b634bb4663e5bd7320439deb4501d
DatViseR <dzdzstepel@gmail.com>
2024-10-21 18:25:07 +0200

    first draft csv upload feature

--- app.R
+++ app.R
@@ -9,50 +9,44 @@
 
 library(shiny)
 
-ui <- fluidPage(
-titlePanel("Upload CSV File"),
-sidebarLayout(
-  sidebarPanel(
-    fileInput("file1", "Upload a csv file with 3 columns: -log10 pvalue,
+ui <- fluidPage(titlePanel("Upload CSV File"),
+                sidebarLayout(
+                  sidebarPanel(
+                    fileInput(
+                      "file1",
+                      "Upload a csv file with 3 columns: -log10 pvalue,
                 log2 fold and single gene names or uniprotIDs",
-              accept = c(
-                "text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv")
-    ),
-    
-    tags$hr(),
-    checkboxInput("header", "Header", TRUE),
-    radioButtons("sep", "Separator",
-                 choices = c(Comma = ",",
-                             Semicolon = ";",
-                             Tab = "\t"),
-                 selected = ","),
-    # add action button to upload the file
-    actionButton("upload", "Upload File"),
-      ),
-  
-  
-  mainPanel(
-    tableOutput("contents")
-  )
-)
-)
+                      accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")
+                    ),
+                    tags$hr(),
+                    checkboxInput("header", "Header", TRUE),
+                    radioButtons(
+                      "sep",
+                      "Separator",
+                      choices = c(
+                        Comma = ",",
+                        Semicolon = ";",
+                        Tab = "\t"
+                      ),
+                      selected = ","
+                    ),
+                    actionButton("upload", "Upload")
+                  ),
+                  
+                  mainPanel(tableOutput("contents"))
+                ))
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-# create an observer for the file input
-output$contents <- renderTable({
-  req(input$file1)
- 
-  in_file <- input$file1
-  df <- read.csv(in_file$datapath, header = input$header, sep = input$sep)
-  head(df)
-})
-
-
+  observeEvent(input$upload, {
+    req(input$file1)
+    in_file <- input$file1
+    df <- read.csv(in_file$datapath,
+                   header = input$header,
+                   sep = input$sep)
+    output$contents <- renderTable({
+      head(df)
+    })
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
