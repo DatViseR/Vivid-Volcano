3773b8139cfdedf9b07129d172443e9a711b2a80
DatViseR <dzdzstepel@gmail.com>
2024-10-22 10:29:29 +0200

    Introduced reactive df that is updated by selected columns and calculated adj.pavalue

--- app.R
+++ app.R
@@ -50,6 +50,9 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  # Reactive value to store the uploaded dataframe
+  uploaded_df <- reactiveVal()
+  
   observeEvent(input$upload, {
     req(input$file1)
     in_file <- input$file1
@@ -57,6 +60,9 @@ server <- function(input, output, session) {
     # Use readr package to read the uploaded dataset
     df <- read_delim(in_file$datapath, delim = input$sep, 
                      col_names = input$header, locale = locale(decimal_mark = input$dec))
+    
+    # Store the dataframe in the reactive value
+    uploaded_df(df)
     
     output$contents <- renderTable({
       head(df)
@@ -73,17 +79,17 @@ server <- function(input, output, session) {
     })
     
     output$dataset_summary <- renderPrint({
-      #output a message to the user "Check if the summary of the data is appropriate "
-      cat("The following columns were uploaded \n \n" )
+      cat("The following columns were uploaded \n \n")
       dplyr::glimpse(df)
     })
   })
   
   observeEvent(input$save_columns, {
     req(input$pvalue_col, input$fold_col, input$annotation_col)
+    
     output$column_info <- renderPrint({
       cat("The following columns were selected to build a volcano plot \n \n")
-        list(
+      list(
         pvalue = input$pvalue_col,
         fold = input$fold_col,
         annotation = input$annotation_col
@@ -93,14 +99,42 @@ server <- function(input, output, session) {
     output$column_structure <- renderPrint({
       cat("The following columns structure was selected to build a volcano plot \n \n")
       
-      req(input$file1)
-      in_file <- input$file1
+      # Use the reactive value to get the dataframe
+      df <- uploaded_df()
       
-      df <- read_delim(in_file$datapath, delim = input$sep, 
-                       col_names = input$header, locale = locale(decimal_mark = input$dec))
-      
-      selected_df <- df[, c(input$pvalue_col, input$fold_col, input$annotation_col)]
-      str(selected_df)
+      # Select the relevant columns
+      selected_columns <- df[, c(input$pvalue_col, input$fold_col, input$annotation_col)]
+      # Update the reactive value with the selected columns dataframe
+      uploaded_df(selected_columns)
+      str(selected_columns)
+    })
+  })
+  
+  observeEvent(input$adjust_pvalues, {
+    req(uploaded_df(), input$pvalue_col)
+    
+    # Use the reactive value to get the selected columns dataframe
+    df <- uploaded_df()
+    
+    # Retrieve the selected p-value column
+    pvalues <- df[[input$pvalue_col]]
+    
+    # Adjust the p-values using the selected method
+    adjusted_pvalues <- p.adjust(pvalues, method = input$adj)
+    
+    # Add the adjusted p-values to the dataframe
+    df$adjusted_pvalues <- adjusted_pvalues
+    
+    # Update the reactive value with the new dataframe
+    uploaded_df(df)
+    
+    output$pvalue_distribution <- renderPrint({
+      summary(adjusted_pvalues)
+    })
+    
+    output$significant_genes <- renderPrint({
+      significant_genes <- df %>% filter(adjusted_pvalues < input$alpha)
+      significant_genes
     })
   })
 }
